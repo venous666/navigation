76634a327de2039c4d0718683e5770364c2e0987
MARIA EDUARDA GARIBALDI DA CRUZ <maria_eg_cruz@estudante.sesisenai.org.br>
2023-06-01 11:39:53 -0300

    veus linda

--- tabs1/app/index.tsx
+++ tabs1/app/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput, TouchableOpacity, Image } from 'react-native';
 import { Link } from "expo-router";
 
 export default function App() {
